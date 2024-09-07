0cb1199e10bf64b2766ed3ee1c8ee5ea68f22e4d
Jade Therras <jade.therras@epfl.ch>
2024-09-07 09:24:21 +0200

    Helpie

--- src/Components/Projects/Helpie/Helpie.js
+++ src/Components/Projects/Helpie/Helpie.js
@@ -4,8 +4,11 @@ import Row from 'react-bootstrap/Row';
 import "../../../Style.css";
 import Card from '../../card';
 import "../../button.css";
+import { Image } from 'react-bootstrap';
 
 import helpie from "./helpie.png"
+import screen from "./screen.png"
+import team from "./team.png"
 import report from "./ATC.pdf"
 
 
@@ -19,44 +22,50 @@ const Helpie = () => {
   const user = "https://github.com/ejayromero/ATC-Helpie-2024/blob/main/HELPIE%20-%20Mode%20d'emploi%20-%20User%20guide.pdf"
   const epfl = "https://actu.epfl.ch/news/making-a-difference-with-the-make-assistive-techno/"
   
+  const HH = "https://www.hackahealth.ch/"
+  const make = "https://www.epfl.ch/education/educational-initiatives/discovery-learning-program-2/interdisciplinary-projects/"
+  const euroteck = "https://euroteq.eurotech-universities.eu/news/euroteqathon-2024-celebrating-connection-through-innovation-and-impact/"
   return (
         <Card
         title="Helpie"
         bodyText="An app to help neurodivergent and mental impaired people use public transport"
         linkText="Learn More"
         cardContent={<div>
-            <p>
-            <br/><br/><br/>
             
+            <br/><br/><br/>
+            <h3>The project</h3>
+            <p>
             Nowerdays in switzerland, public transport can get you nearly everywhere.
             We use it daily, to go to work, see friends, make groceries...
             We use the SBB app, the swiss transport app, to buy the ticket, plan and follow the course. 
             However, the use of such complex app can be difficult and stressing for some peoples. Delay and others can be additional stress.
-            <br/><br/>
-            Helpie have been developped within the Hackaheath program, in collaboration with the SBB and SBB inclusive. 
-
-            Helpie, an app to help mental impaired people to travel alone 
-            This project is proposed in collaboration with SBB. 
-            The goal is to develop an app that helps mental impaired people to travel serenely alone. 
-            SBB inclusive app team will provide support to get and provide simplified information from their app. 
-            The challenger will specify his needs to personalise the solution
+            This is where Helpie come from. 
+            </p><p>
+            Helpie have been developped within the HackaHealth program, in collaboration with the SBB and SBB inclusive. The developpement process have been
+            conducted in collaboration with a real life potential user.<br/>
+            The project have been selected to participate to the 2024 EuroTeQaThon in Paris.
           </p>
-          <Col>
+          <h3>Technical overview</h3>
+          <p>Prototyped in Figma, developped in Android studio. </p> <br/>
           <Row className="justify-content-md-center gap-1">
-              <div><p>
-                <br/>
-                <br/>
+          <Col align-items= "center">
+              <p>
                 6 months projects<br/>
-                <a href>Hackaheath association</a><br/>
+                <a href>HackaHealth association</a><br/>
                 <a href>MAKE epfl project</a><br/>
                 <a href>SSB</a><br/>
                 <a href>Euroteck hackatlon</a><br/></p>
-                </div>
-                </Row>
                 </Col>
+                <Col>
+                <Image src={screen} alt="Home screen of Helpie" width="40%" rounded fluid className="position-relative z-index-1" />
+                </Col>
+                </Row>
+                <br/><br/>
+                <Image src={team} alt="EPFL ATC team at EuroTeQ" rounded fluid className="position-relative z-index-1" />
+                <br/><br/>
                 <div>
                 <button class="button-48" role="button" onClick={() =>window.open(git)}><span class="text"><FaGithub /> Go to git repository</span></button><br/>
-                <button class="button-48" role="button" onClick={() =>window.open(report)}><span class="text"><FaFileAlt />Get project report</span></button><br/>
+                <button class="button-48" role="button" onClick={() =>window.open(report)}><span class="text"><FaFileAlt />See project report</span></button><br/>
                 <button class="button-48" role="button" onClick={() =>window.open(user)}><span class="text"><RiGuideFill />See user guide</span></button><br/>
                 <button class="button-48" role="button" onClick={() =>window.open(epfl)}><span class="text"><MdArticle />See epfl article</span></button><br/>
                 </div>
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,28 +1,49 @@
-import React, { useState } from 'react';
-import "./card.css";
+import React, { useState, useEffect } from 'react';
+import './card.css';
 
-const Card = ({ title, bodyText, linkText,cardContent,backgroundImage }) => {
-
-  const [isExpanded, setIsExpanded] = useState(false);
+const Card = ({ title, bodyText, linkText, cardContent, backgroundImage }) => {
+  const [Expansion, setExpand] = useState(false); // Track card expansion state
+  const [isExpanded, setIsExpanded] = useState(false); // Track card expansion state
+  const [showBody, setShowBody] = useState(false);     // Control showing of the body text after expansion
+  
   const handleExpandClick = () => {
-    setIsExpanded(!isExpanded);
+    if (isExpanded) {
+      // If collapsing, reset states immediately
+      setShowBody(false);
+      setIsExpanded(false);
+      setExpand(false)
+    } else {
+      setExpand(true);
+      setTimeout(() => {
+        setIsExpanded(true);
+      }, 1500);
+      setTimeout(() => {
+        setShowBody(true);   // Step 3: Show the body after card expands
+      }, 5000);
+    }
   };
 
-    return (
-      <div className={`card ${isExpanded ? 'expanded' : ''}`}
-      style={{
-        "--background-image": `url(${backgroundImage})`,
-      }}>
-        <div className="card-content">
-          <h2 className="card-title">{title}</h2>
-          <p className="card-body">{bodyText}</p>
-          <div className="card-content-container">{cardContent}</div>
-          <button className="button" onClick={handleExpandClick}>
-            {isExpanded ? "Collapse" : linkText}
-          </button>
-        </div>
+  return (
+    <div
+      className={`card ${isExpanded ? 'expanded' : ''}`}
+      style={{ '--background-image': `url(${backgroundImage})` }}
+    >
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className={`card-body ${Expansion ? (showBody ? 'visible' : 'invisible') : ''}`}>{bodyText}</p>
+
+        {isExpanded && (
+          <div className={`card-content-container ${isExpanded ? (showBody ? 'visible' : 'invisible') : ''}`}>
+            {cardContent}
+          </div>
+        )}
+
+        <button className="button" onClick={handleExpandClick}>
+          {isExpanded ? 'Collapse' : linkText}
+        </button>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Card;
+export default Card;

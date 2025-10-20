a458801135f7fa755dbef36e772fdc8a6f74890e
Jade Therras <jade.therras@epfl.ch>
2025-10-20 14:42:38 +0200

    new cv updated and supplément, icons regrouped

---
+++ src/icons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// icons.js
+import { FaGithub, FaVideo, FaFileAlt } from "react-icons/fa";
+import { GiTargetPrize, GiHealthNormal,GiConsoleController } from "react-icons/gi";
+import { CgWebsite } from "react-icons/cg";
+import { PiPresentationChart } from "react-icons/pi";
+import { ImLab } from "react-icons/im";
+import { RiGuideFill } from "react-icons/ri";
+import { MdArticle } from "react-icons/md";
+import { TfiLayoutSlider } from "react-icons/tfi";
+
+export {
+  FaGithub,
+  FaVideo,
+  FaFileAlt,
+  GiTargetPrize,
+  GiHealthNormal,
+  GiConsoleController,
+  CgWebsite,
+  PiPresentationChart,
+  ImLab,
+  RiGuideFill,
+  MdArticle,
+  TfiLayoutSlider,
+};
